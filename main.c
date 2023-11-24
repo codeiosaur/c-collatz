8f29af325eef0377da1a59ee603677ab51297259
codeiosaur <codeiosaur@outlook.com>
2023-11-23 20:35:46 -0800

    Made MANY refactoring changes
    - Removed everything unsigned int related, as it gave almost no memory savings and it only complicated the code (helped for about 150,000 integers, and saved ~600-700 KB)
    - mass_uint_collatz() is still present but unused and deprecated
    - Modified L1_CPU_CACHE to properly reflect my system (Ryzen 9 5900x)
    - Added an L2_CPU_CACHE to provide another way to optimize the system
    - Added a return statement so that main properly returns
    - Made Struct_ulonglong internals more concise
    - THE BIG ONE: Added a static array in order to optimize the code and allow you to not compute a number more than once.

--- main.c
+++ main.c
@@ -1,53 +1,41 @@
+#include <math.h>
 #include <stdio.h>
-#include <limits.h>
 
-#define L1_CACHE_SIZE = 32000 // amount of unsigned ints that can fit into my L1 CPU cache - change as needed
+#define L1_CACHE_SIZE 8192 // My l1 cache size/core is 64 KB, and one struct is 8 bytes. 8192 structs can fit - change as needed
+#define L2_CACHE_SIZE 65536 // My l2 cache size/core is 512 KB - multiply l1 by 7
 
-struct uintCollatzStepInfo {
-	unsigned int step_count, max;
-};
-
-struct ulonglongCollatzStepInfo {
-	unsigned long long step_count, max;
-};
-
-typedef struct uintCollatzStepInfo Struct_uint;
-typedef struct ulonglongCollatzStepInfo Struct_ulonglong;
-
-Struct_uint uint_collatz_struct(unsigned int collatz) {
-	Struct_uint info;
-	info.max = 0;
-	info.step_count = 0;
-	while (collatz != 1) {
-		if (collatz >= info.max) {
-			info.max = collatz;
-		}
-		if (collatz % 2 == 0) {
-			collatz /= 2;
-		} else {
-			collatz = (3 * collatz) + 1;
-		}
-		printf("%u\n", collatz);
-		info.step_count++;
-	}
-	return info;
-}
+typedef struct ulonglongCollatzStepInfo {
+	unsigned long long steps, max;
+} Struct_ulonglong;
 
 Struct_ulonglong ulonglong_collatz_struct(unsigned long long collatz) {
-	Struct_ulonglong info;
-	info.max = 0;
-	info.step_count = 0;
-	while (collatz != 1) {
-		if (collatz >= info.max) {
-			info.max = collatz;
+	// PRECONDITION: collatz > 0
+	unsigned long long original_collatz = collatz;
+	static Struct_ulonglong collatz_result[L2_CACHE_SIZE] = {{0, 0}}; // Array to optimize program by storing precalculated steps
+	Struct_ulonglong info = { 0, collatz };
+	
+	while (collatz > 1) {
+		if (collatz < L2_CACHE_SIZE && collatz_result[collatz].steps != 0) {
+			info.steps += collatz_result[collatz].steps;
+			if (collatz_result[collatz].max > info.max) {
+				info.max = collatz_result[collatz].max;
+			}
+			break;
 		}
 		if (collatz % 2 == 0) {
 			collatz /= 2;
 		} else {
 			collatz = (3 * collatz) + 1;
 		}
-		printf("%llu\n", collatz);
-		info.step_count++;
+		//printf("%llu\n", collatz);
+		info.steps++;
+		if (collatz >= info.max) {
+			info.max = collatz;
+		}
+	}
+
+	if (original_collatz < L2_CACHE_SIZE && collatz_result[original_collatz].steps == 0) {
+		collatz_result[original_collatz] = info; // Cache the calculated steps for future use
 	}
 	return info;
 }
@@ -56,7 +44,7 @@ void mass_uint_collatz(unsigned int end) {
 	// used to find the point at which the uint function overflows - it overflows at 159,487
 	for (unsigned int i = 2; i < end; i++) {
 		unsigned int collatz = i;
-		while (collatz != 1) {
+		while (collatz > 1) {
 			if (collatz % 2 == 0) {
 				collatz /= 2;
 			} else if ((3 * collatz + 1) < collatz) {
@@ -70,25 +58,16 @@ void mass_uint_collatz(unsigned int end) {
 }
 
 int main() {
-	unsigned long long val = 989345275647;
-	if (val < 159000) { // approximation
-		printf("%u\n", val);
-		Struct_uint result = uint_collatz_struct((unsigned int)val);
-		printf("Number of Steps: %u\n", result.step_count);
-		printf("Highest Step Reached: %u\n", result.max);
-	} else {
-		printf("%llu\n", val);
-		Struct_ulonglong result = ulonglong_collatz_struct(val);
-		printf("Number of Steps: %llu\n", result.step_count);
-		printf("Highest Step Reached: %llu\n", result.max);
-	}
-	//printf("%u\n", 159487);
-	//Struct_uint answer = uint_collatz_struct(159487);
-	//printf("test: %d\n", answer);
+	unsigned long long val1 = 16;
+	Struct_ulonglong result = ulonglong_collatz_struct(val1);
+	printf("%llu\n", val1);
+	printf("Number of Steps: %llu\n", result.steps);
+	printf("Highest Step Reached: %llu\n", result.max);
 
-	//unsigned int val = 77031;
-	//Struct_uint result = uint_collatz_struct(val);
-	//printf("%d\n", val);
-	//printf("Number of Steps: %u\n", result.step_count);
-	//printf("Highest Step Reached: %u\n", result.max);
+	unsigned long long val2 = 113383;
+	result = ulonglong_collatz_struct(val2);
+	printf("%llu\n", val2);
+	printf("Number of Steps: %llu\n", result.steps);
+	printf("Highest Step Reached: %llu\n", result.max);
+	return 0;
 }
